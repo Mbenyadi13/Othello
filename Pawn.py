42a579629991bf8d2253db94e1e0b0a1d1b2e8f6
Melanie Benyadi <melanie.benyadi@le-campus-numerique.fr>
2025-08-12 12:57:07 +0200

    pawn

---
+++ Pawn.py
@@ -0,0 +1,10 @@
+class Pawn:
+    def __init__(self,color):
+        self.color = color
+
+    def flippawn(self):
+        if self.color == "black":
+            self.color == "white"
+        else:
+            self.color == "black"
+        
