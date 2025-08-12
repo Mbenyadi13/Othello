39651fbb05130847ee122d0e39e516aefeabcb3e
Melanie Benyadi <melanie.benyadi@le-campus-numerique.fr>
2025-08-12 13:22:28 +0200

    Pawn _updates

--- Pawn.py
+++ Pawn.py
@@ -1,10 +1,26 @@
+from enum import Enumnum
+class color(Enum):
+    BLACK= "black"
+    WHITE= "white"
+
+print(color.BLACK)
+print(color.WHITE)
+print(color.WHITE ==color.BLACK)
+print(color.BLACK == color.WHITE)
+
+
+
 class Pawn:
     def __init__(self,color):
-        self.color = color
+        self._color = color
+
+    @property 
+    def color(self):
+        return self._color
 
     def flippawn(self):
-        if self.color == "black":
-            self.color == "white"
+        if self._color == color.BLACK:
+            self._color == color.WHITE
         else:
-            self.color == "black"
+            self._color == color.BLACK
         
